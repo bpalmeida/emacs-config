7a066e3a3fd4db5305c81d1f044d091f8ac804f6
Bernardo Almeida <bernardo.pinto.almeida@gmail.com>
2020-12-05 21:29:02 +0000

    adding format-all

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
   
   (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                            ("melpa" . "https://melpa.org/packages/")
-                           ("melpa-stable" . "https://stable.melpa.org/packages/")
+                ;           ("melpa-stable" . "https://stable.melpa.org/packages/")
                            ("org" . "http://orgmode.org/elpa/")))
 
   (setq-default
